d8b0991176e67808d5c8dbdf692e42421d867c97
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-10-31 12:21:53 +0530

    added quiz schemas

---
+++ models/quizzes.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Quiz struct {
+	Title     string             `json:"title" validate:"required"`
+	Created   string             `json:"created" validate:"required"`
+	Creator   primitive.ObjectID `json:"creator" validate:"required"`
+	Questions []Quiz             `json:"questions" validate:"required"`
+}
+
+type Question struct {
+	Prompt  string   `json:"prompt" validate:"required"`
+	Answers []string `json:"answers" validate:"required"`
+	Correct int      `json:"correct" validate:"required"`
+}
