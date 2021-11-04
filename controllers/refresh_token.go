dd9fb38c9056d10fad3df95ac62da5632273e02a
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-11-04 16:36:17 +0530

    updated refresh token controller

--- controllers/refresh_token.go
+++ controllers/refresh_token.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Tutor2Tutee/T2T-GO/helpers"
@@ -10,15 +11,30 @@ import (
 
 func RefreshToken(c *gin.Context) {
 	refreshToken := c.Request.Header.Get("Refresh_token")
+	if c.Request.Header["Refresh_token"] == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "No refresh token found in the headers"})
+		return
+	}
+
 	to, err := helpers.VerifyToken(refreshToken)
 	if err != nil {
 		// in case token is expired
 		c.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
-		c.Abort()
 		return
 	}
-	if claims, ok := to.Claims.(jwt.MapClaims); ok && to.Valid {
-		c.JSON(http.StatusAccepted, claims)
-	}
+
+	claims, _ := to.Claims.(jwt.MapClaims)
+
+	// Verify
+	//
+
+	// Return new token
+	email := fmt.Sprintf("%v", claims["Email"])
+	nickname := fmt.Sprintf("%v", claims["Nickname"])
+	userID := fmt.Sprintf("%v", claims["UserID"])
+
+	token := helpers.GenerateAccessToken(email, nickname, userID)
+
+	c.JSON(http.StatusCreated, gin.H{"message": "generated new token successfully", "token": token})
 
 }
