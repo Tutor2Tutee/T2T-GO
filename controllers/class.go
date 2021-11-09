aa36019cb5a6859d60a0fcab9e85cec8a1ab4e70
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-11-09 19:09:58 +0530

    added JWT auth to /api/classes

--- controllers/class.go
+++ controllers/class.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
-	"github.com/Tutor2Tutee/T2T-GO/repository"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 	"time"
+
+	"github.com/Tutor2Tutee/T2T-GO/helpers"
+	"github.com/Tutor2Tutee/T2T-GO/repository"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/Tutor2Tutee/T2T-GO/models"
 	"github.com/gin-gonic/gin"
@@ -42,6 +44,7 @@ func (CC ClassController) GetOne(c *gin.Context) {
 func (CC ClassController) Create(c *gin.Context) {
 	var class models.Class
 	err := c.BindJSON(&class)
+
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
@@ -50,6 +53,23 @@ func (CC ClassController) Create(c *gin.Context) {
 	}
 	class.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 	class.Listener = []models.User{}
+
+	// Find user with user Email
+	user, err := repository.UserCollection.FindUserByEmail(class.Teacher.Email)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// JWT Verification
+	isAuthenticated, _ := helpers.VerifyUserAuthUsingJWT(c, user.ID.Hex())
+
+	if !isAuthenticated {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized User"})
+		return
+	}
 
 	result, err := repository.ClassCollection.InsertClass(class)
 	if err != nil {
--- routers/classes.go
+++ routers/classes.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/Tutor2Tutee/T2T-GO/controllers"
+	"github.com/Tutor2Tutee/T2T-GO/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,8 +10,8 @@ func classesRouterInit(r *gin.RouterGroup) {
 	//classController := controllers.Class()
 	classes := r.Group("/classes")
 	{
-		classes.GET("", controllers.ClassController{}.GetAll)
-		classes.POST("", controllers.ClassController{}.Create)
-		classes.GET("/:cid", controllers.ClassController{}.GetOne)
+		classes.GET("", middlewares.JWTAuthenticationMiddleware, controllers.ClassController{}.GetAll)
+		classes.POST("", middlewares.JWTAuthenticationMiddleware, controllers.ClassController{}.Create)
+		classes.GET("/:cid", middlewares.JWTAuthenticationMiddleware, controllers.ClassController{}.GetOne)
 	}
 }
